f7332b116195a0b423f71ff335b85913939f70d6
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-01-19 18:58:33 +0100

    Home test view

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from main.views import test_view
+from main.views import test_view, home
 
 
 urlpatterns = [
+    path('', home),
     path('test/', test_view),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, HttpResponse
 
 
+def home(request):
+    return HttpResponse('home_view')
+
 def test_view(request):
     return HttpResponse('test_view')
