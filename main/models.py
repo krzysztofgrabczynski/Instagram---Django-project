d593b01127a603a3a756f9c1f6a551f7c85ca992
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-01-19 19:09:18 +0100

    New class User in models.py

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    GENDER = (
+        (1, 'male'),
+        (2, 'female')
+    )
+
+    first_name = models.CharField(null=False, blank=False, max_length=30)
+    last_name = models.CharField(null=False, blank=False, max_length=30)
+    email = models.EmailField(null=False, blank=False, max_length=30)
+    gender = models.CharField(null=False, blank=False, max_length=6, choices=GENDER, default=1)
+    
+
