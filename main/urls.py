4560d8cad2b4779d11ecdbfe65d15c8bb83a8c16
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-01-22 21:47:04 +0100

    New path

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from main.views import test_view, home, login
+from main.views import test_view, home, sign_up, login
 
 
 
 urlpatterns = [
     path('', home, name='home'),
     path('test/', test_view),
+    path('sign_up/', sign_up, name='sign_up'),
     path('login/', login, name='login'),
 ]
