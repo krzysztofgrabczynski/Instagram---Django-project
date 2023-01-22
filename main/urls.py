6d03e1a1765da61d0a746e36494590639b0c39f2
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-01-22 21:33:12 +0100

    Updating urls, adding names and new path

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from main.views import test_view, home
+from main.views import test_view, home, login
+
 
 
 urlpatterns = [
-    path('', home),
+    path('', home, name='home'),
     path('test/', test_view),
+    path('login/', login, name='login'),
 ]
