5f809cc07b108150a78d2495a64b5dbef1a7315c
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-01-22 21:46:27 +0100

    Adding new setting

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrapform',
     'main',
 ]
 
@@ -113,12 +114,9 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
