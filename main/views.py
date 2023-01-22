abd553c793ad992fb5ff74e37c17015261527881
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-01-23 00:03:23 +0100

    Adding decorator: login_required

--- main/views.py
+++ main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegistrationForm
 
 
+@login_required
 def home(request):
     return render(request, 'home.html')
 
