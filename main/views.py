426a2d5a8b6c19f34da39a63f38371c923c099df
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-01-22 21:48:05 +0100

    Login and Sign_up view in views.py

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from .forms import UserRegistrationForm
 
 
 def home(request):
@@ -6,3 +7,14 @@ def home(request):
 
 def test_view(request):
     return HttpResponse('test_view')
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        return redirect('home')
+    else:
+        form = UserRegistrationForm()
+        return render(request, 'registration/sign_up.html', {'form': form})
+
+def login(request):
+    return render(request, 'registration/login.html')
