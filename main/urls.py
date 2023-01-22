f77a38a0c137b043f51c3a9cbd882f731824d42c
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-01-22 22:07:25 +0100

    New paths, login and logout using django views

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from main.views import test_view, home, sign_up, login
+from main.views import test_view, home, sign_up
+from django.contrib.auth import views as auth_views
 
 
 
@@ -7,5 +8,6 @@ urlpatterns = [
     path('', home, name='home'),
     path('test/', test_view),
     path('sign_up/', sign_up, name='sign_up'),
-    path('login/', login, name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
