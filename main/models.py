d5427beeb9a9a695556d4b639943a9b424356be9
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-01-22 21:46:52 +0100

    Refactor

--- main/models.py
+++ main/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 
 
-class User(models.Model):
-    GENDER = (
-        (1, 'male'),
-        (2, 'female')
-    )
 
-    first_name = models.CharField(null=False, blank=False, max_length=30)
-    last_name = models.CharField(null=False, blank=False, max_length=30)
-    email = models.EmailField(null=False, blank=False, max_length=30)
-    gender = models.CharField(null=False, blank=False, max_length=6, choices=GENDER, default=1)
+# class User(models.Model):
+#     GENDER = (
+#         (1, 'male'),
+#         (2, 'female')
+#     )
+
+#     first_name = models.CharField(null=False, blank=False, max_length=30)
+#     last_name = models.CharField(null=False, blank=False, max_length=30)
+#     email = models.EmailField(null=False, blank=False, max_length=30)
+#     gender = models.CharField(null=False, blank=False, max_length=6, choices=GENDER, default=1)
     
 
