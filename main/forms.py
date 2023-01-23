bd0d203d8e84e572be70fe46b32c6080817cb07d
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-01-23 18:26:12 +0100

    Adding new filed in RegistrationForm: gender

--- main/forms.py
+++ main/forms.py
@@ -4,10 +4,15 @@ from django import forms
 
 
 class UserRegistrationForm(UserCreationForm):
+    GENDER = [
+        (0, 'male'),
+        (1, 'female')
+    ]
+
     first_name = forms.CharField(max_length=30, required=True)
     last_name = forms.CharField(max_length=30, required=True)
     email = forms.EmailField(max_length=254, required=True)
-
+    gender = forms.ChoiceField(choices=GENDER, required=True)
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'gender', 'password1', 'password2']
