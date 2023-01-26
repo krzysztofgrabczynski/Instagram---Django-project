02538a9803b96a5dcb6b47c1666079299e852336
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-01-26 23:47:28 +0100

    Refactor

--- main/forms.py
+++ main/forms.py
@@ -14,10 +14,10 @@ class UserRegistrationForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True)
     last_name = forms.CharField(max_length=30, required=True)
     email = forms.EmailField(max_length=254, required=True)
-    gender = forms.ChoiceField(choices=GENDER, required=True)
+    # gender = forms.ChoiceField(choices=GENDER, required=True)
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'gender', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
 
 class UserProfileForm(ModelForm):
     class Meta:
