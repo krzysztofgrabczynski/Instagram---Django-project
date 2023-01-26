8cc6df1e28f171acb48b004120fe6e3479f6c766
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-01-26 12:34:10 +0100

    Adding __str__ and __repr__ in UserProfile model

--- main/models.py
+++ main/models.py
@@ -9,3 +9,9 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     description = models.TextField(default='')
     profile_img = models.ImageField(upload_to='profile_imgs', blank=True, null=True)
+
+    def __str__(self) -> str:
+        return f'Profile: {self.user.username}'
+
+    def __repr__(self) -> str:
+        return self.__str__()
