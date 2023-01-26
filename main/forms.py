31c6a7bfc10084c471c1a23a47d018f34fb3efd8
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-01-26 12:24:40 +0100

    Editing of the fields in UserRegistrationForm

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import UserProfile
 from django import forms
+from django.forms import ModelForm
 
 
 class UserRegistrationForm(UserCreationForm):
@@ -16,3 +18,8 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'gender', 'password1', 'password2']
+
+class UserProfileForm(ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['user', 'description', 'profile_img']
