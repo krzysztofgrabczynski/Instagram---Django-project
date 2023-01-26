e5bca1f7092111c8a3e13cad7c606e7cff3cb0ca
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-01-26 12:23:35 +0100

    Sign up func refactor

--- main/views.py
+++ main/views.py
@@ -1,21 +1,29 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserProfileForm
 
 
 @login_required
 def home(request):
     return render(request, 'home.html')
 
+
 def sign_up(request):
     if request.method == 'POST':
-        form = UserRegistrationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-        return redirect('home')
+        user_registration_form = UserRegistrationForm(request.POST or None)
+        user_profile_form = UserProfileForm()
+        if user_registration_form.is_valid():
+            user_profile = user_profile_form.save(commit=False)
+            user = user_registration_form.save()
+            user_profile.user = user
+            user_profile.save()
 
-    form = UserRegistrationForm()
-    return render(request, 'registration/sign_up.html', {'form': form})
+            login(request, user)
+            return redirect('home')
+
+        else:
+            print(user_registration_form.errors)
+    user_registration_form = UserRegistrationForm()
+    return render(request, 'registration/sign_up.html', {'form': user_registration_form})
 
