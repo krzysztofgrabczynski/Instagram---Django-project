cc09e8ce6c960131a2174cfe4a5c2e309a7da234
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-01-26 12:23:16 +0100

    Updating UserProfile Model

--- main/models.py
+++ main/models.py
@@ -1,16 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
+User = get_user_model()
 
-# class User(models.Model):
-#     GENDER = (
-#         (1, 'male'),
-#         (2, 'female')
-#     )
 
-#     first_name = models.CharField(null=False, blank=False, max_length=30)
-#     last_name = models.CharField(null=False, blank=False, max_length=30)
-#     email = models.EmailField(null=False, blank=False, max_length=30)
-#     gender = models.CharField(null=False, blank=False, max_length=6, choices=GENDER, default=1)
-    
-
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    description = models.TextField(default='')
+    profile_img = models.ImageField(upload_to='profile_imgs', blank=True, null=True)
