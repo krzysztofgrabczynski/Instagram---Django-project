6511ce2455814ac0373a427101c19f354f86bf33
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-01-27 15:40:05 +0100

    Adding new path to edit_profile

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import home, sign_up, edit_account
+from main.views import home, sign_up, edit_account, edit_profile
 from django.contrib.auth import views as auth_views
 
 
@@ -9,6 +9,7 @@ urlpatterns = [
     path('sign_up/', sign_up, name='sign_up'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('edit_account/<int:id>', edit_account, name='edit_account')
+    path('edit_account/<int:id>', edit_account, name='edit_account'),
+    path('edit_profile/<int:id>', edit_profile, name='edit_profile')
 
 ]
