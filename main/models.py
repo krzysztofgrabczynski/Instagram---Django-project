92068a0068920f8996694771a2e72cd79e2f5eb4
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-01-27 16:01:03 +0100

    Refactor

--- main/models.py
+++ main/models.py
@@ -6,7 +6,13 @@ User = get_user_model()
 
 
 class UserProfile(models.Model):
+    GENDER = [
+        (1, 'male'),
+        (2, 'female'),
+    ]
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    gender = models.PositiveSmallIntegerField(choices=GENDER, default=1)
     description = models.TextField(default='')
     profile_img = models.ImageField(upload_to='profile_imgs', blank=True, null=True)
 
