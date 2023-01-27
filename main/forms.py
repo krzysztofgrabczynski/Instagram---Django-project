e6ad14ca2fc947f2682b062ff10df2a68e5e8cc8
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-01-27 22:50:40 +0100

    Deleting of the gender field in UserRegistrationForm

--- main/forms.py
+++ main/forms.py
@@ -14,7 +14,6 @@ class UserRegistrationForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True)
     last_name = forms.CharField(max_length=30, required=True)
     email = forms.EmailField(max_length=254, required=True)
-    # gender = forms.ChoiceField(choices=GENDER, required=True)
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
