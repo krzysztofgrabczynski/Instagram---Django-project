12338f37ea8999ef75b2a47ba585767476bc799e
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-01-30 23:48:08 +0100

    Registratrion of the new models

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, Post, Comment
 
 
 admin.site.register(UserProfile)
+admin.site.register(Post)
+admin.site.register(Comment)
+
 
