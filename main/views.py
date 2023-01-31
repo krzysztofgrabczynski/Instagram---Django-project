7f48ca3318342e96f3054752b1949b06f5f383cb
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-01-31 11:17:39 +0100

    Updating user_profile view

--- main/views.py
+++ main/views.py
@@ -67,5 +67,6 @@ def edit_profile(request, id):
 def user_profile(request, id):
     profile = UserProfile.objects.get(id=id)
     gender = profile.GENDER[profile.gender][1]
+    posts = profile.user.posts.all()
 
-    return render(request, 'user_profile.html', {'profile': profile, 'gender': gender})
+    return render(request, 'user_profile.html', {'profile': profile, 'gender': gender, 'posts': posts})
