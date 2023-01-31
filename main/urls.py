70fb274d544dc4cd05653ea38d600316da13d637
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-01-31 13:39:00 +0100

    New url-add_post

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import home, sign_up, edit_account, edit_profile, user_profile
+from main.views import home, sign_up, edit_account, edit_profile, user_profile, add_post
 from django.contrib.auth import views as auth_views
 
 
@@ -13,5 +13,6 @@ urlpatterns = [
     path('edit_account/<int:id>', edit_account, name='edit_account'),
     path('edit_profile/<int:id>', edit_profile, name='edit_profile'),
     path('profile/<int:id>', user_profile, name='user_profile'),
+    path('add_post/', add_post, name='add_post'),
 
 ]
