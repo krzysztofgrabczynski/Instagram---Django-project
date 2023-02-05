e283a60fe094f02eca4d2c6b9675b0f3beeca88a
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-02-05 21:51:06 +0100

    Deleting of the like_hover.js

--- static_dir/like_hover.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('#element').addEventListener('mouseover', function () {
-	document.querySelector('#additional-content').style.display = 'block';
-});
-
-document.querySelector('#element').addEventListener('mouseout', function () {
-	document.querySelector('#additional-content').style.display = 'none';
-});
