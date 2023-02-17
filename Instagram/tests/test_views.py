2ac286d257f4a6641b7c722cf8515cdd98527a7b
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-02-17 12:54:38 +0100

    New tests in test_views.py

--- Instagram/tests/test_views.py
+++ Instagram/tests/test_views.py
@@ -8,7 +8,25 @@ import json
 class TestViews(TestCase):
     def setUp(self):
         self.client = Client()
-        
+        self.user1 = User.objects.create_user(
+            username='David', 
+            first_name='David',
+            last_name='Jones',
+            email='david@example.com',
+            password='PasswordExample123!'
+        )
+        self.user1_profile = UserProfile.objects.create(user=self.user1)
+        self.logged_in = self.client.login(username=self.user1.username, password='PasswordExample123!')
+
+    def test_loggin_user(self):
+        self.assertTrue(self.logged_in)
+
+    def test_views_home_GET(self):
+        response = self.client.get(reverse('home'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+
     def test_views_sign_up_GET(self):
         response = self.client.get(reverse('sign_up'))
 
@@ -26,12 +44,13 @@ class TestViews(TestCase):
         })
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(User.objects.first().first_name, 'John')
-        self.assertEqual(UserProfile.objects.first().user.username, 'Johnny')
+        self.assertEqual(User.objects.get(id=2).first_name, 'John')
+        self.assertEqual(UserProfile.objects.get(id=2).user.username, 'Johnny')
 
     def test_views_sign_up_POST_incorrect_data(self):
         response = self.client.post(reverse('sign_up'))
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(User.objects.count(), 0)
-        self.assertEqual(UserProfile.objects.count(), 0)
+        self.assertTemplateUsed(response, 'registration/sign_up.html')
+        self.assertEqual(User.objects.count(), 1) # value=1, because of creation user1 in setUp
+        self.assertEqual(UserProfile.objects.count(), 1) # value=1, because of creation user1_profile in setUp
