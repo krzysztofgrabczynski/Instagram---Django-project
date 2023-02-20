31e1ccfd7e0066589e2e344028dcc7287ad09ce3
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-02-20 21:43:28 +0100

    Testing add_post view

--- Instagram/tests/test_views.py
+++ Instagram/tests/test_views.py
@@ -17,6 +17,15 @@ class TestViews(TestCase):
         self.test_user_1_profile = UserProfile.objects.create(user=self.test_user_1)
         self.logged_in = self.client.login(username=self.test_user_1.username, password='PasswordExample123!')
 
+        self.test_user_2 = User.objects.create_user(
+            username='test_username_1', 
+            first_name='test_first_name_1',
+            last_name='test_last_name_1',
+            email='test1@example.com',
+            password='PasswordExample123!'
+        )
+        self.test_user_2_profile = UserProfile.objects.create(user=self.test_user_2)
+
     def test_loggin_user(self):
         self.assertTrue(self.logged_in)
 
@@ -34,16 +43,8 @@ class TestViews(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
     def test_views_home_context(self):
-        test_user_2 = User.objects.create_user(
-            username='test_username_1', 
-            first_name='test_first_name_1',
-            last_name='test_last_name_1',
-            email='test1@example.com',
-            password='PasswordExample123!'
-        )
-        test_user_2_profile = UserProfile.objects.create(user=test_user_2)
-        test_post = Post.objects.create(user=test_user_2, description='test description')
-        Follow.objects.create(user=self.test_user_1, user_followed=test_user_2, followd_user_id=test_user_2.id)
+        test_post = Post.objects.create(user=self.test_user_2, description='test description')
+        Follow.objects.create(user=self.test_user_1, user_followed=self.test_user_2, followd_user_id=self.test_user_2.id)
         test_comment = Comment.objects.create(post=test_post, user=self.test_user_1, text='test comment')
 
         response = self.client.get(reverse('home'))
@@ -73,8 +74,8 @@ class TestViews(TestCase):
         })
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(User.objects.get(id=2).first_name, 'John')
-        self.assertEqual(UserProfile.objects.get(id=2).user.username, 'Johnny')
+        self.assertEqual(User.objects.get(id=3).first_name, 'John')
+        self.assertEqual(UserProfile.objects.get(id=3).user.username, 'Johnny')
 
     def test_views_sign_up_POST_incorrect_data(self):
         self.client.logout()
@@ -82,8 +83,8 @@ class TestViews(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'registration/sign_up.html')
-        self.assertEqual(User.objects.count(), 1) # value=1, because of creation user1 in setUp
-        self.assertEqual(UserProfile.objects.count(), 1) # value=1, because of creation user1_profile in setUp
+        self.assertEqual(User.objects.count(), 2) # value=2, because of creation users in setUp
+        self.assertEqual(UserProfile.objects.count(), 2) # value=2, because of creation profiles in setUp
 
     # tests for edit_account view
     def test_views_edit_account_if_not_logged_in(self):
@@ -127,7 +128,7 @@ class TestViews(TestCase):
         self.assertTrue(response.url.startswith, '/instagram/login/?next=/instagram/edit_profile/')
 
     def test_views_edit_profile_GET_404(self):
-        response = self.client.get(reverse('edit_profile', kwargs={'id': 2}))
+        response = self.client.get(reverse('edit_profile', kwargs={'id': 10}))
 
         self.assertEqual(response.status_code, 404)
 
@@ -141,8 +142,6 @@ class TestViews(TestCase):
         response = self.client.post(reverse('edit_profile', kwargs={'id': self.test_user_1.id}), {
             'gender': '1',
             'description': 'Testing',
-            'profile_img': 'profile_imgs/default_male.jpg'
-
         })
 
         user_profile = UserProfile.objects.get(id=1)
@@ -171,3 +170,64 @@ class TestViews(TestCase):
         response = self.client.get(reverse('user_profile', kwargs={'id': self.test_user_1.id}))
 
         self.assertTrue(response.url.startswith, '/instagram/login/?next=/instagram/profile/')
+
+    def test_views_user_profile_GET(self):
+        response = self.client.get(reverse('user_profile', kwargs={'id': self.test_user_1.id}))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'user_profile.html')
+
+    def test_views_user_profile_GET_context(self):
+        Post.objects.create(user=self.test_user_1, description='test description')
+        response = self.client.get(reverse('user_profile', kwargs={'id': self.test_user_1.id}))
+
+        self.assertEqual(response.context['profile'], self.test_user_1_profile)
+        self.assertEqual(response.context['gender'], 'male')
+        self.assertEqual(list(response.context['posts']), list(self.test_user_1.posts.all()))
+    
+    def test_view_user_profile_follow_context_postivie(self):
+        Follow.objects.create(user=self.test_user_1, user_followed=self.test_user_2, followd_user_id=self.test_user_2.id)
+        response = self.client.get(reverse('user_profile', kwargs={'id': self.test_user_2.id}))
+
+        self.assertTrue(response.context['is_followed'])
+    
+    def test_view_user_profile_follow_context_negative_v1(self):
+        response = self.client.get(reverse('user_profile', kwargs={'id': self.test_user_1.id}))
+
+        self.assertFalse(response.context['is_followed'])
+    
+    def test_view_user_profile_follow_context_negative_v2(self):
+        response = self.client.get(reverse('user_profile', kwargs={'id': self.test_user_2.id}))
+
+        self.assertFalse(response.context['is_followed'])
+
+    # test for add_post view
+    def test_views_add_post_if_not_logged_in(self):
+            self.client.logout()
+            response = self.client.get(reverse('add_post'))
+
+            self.assertTrue(response.url.startswith, '/instagram/login/?next=/instagram/profile/add_post')
+
+    def test_view_add_post_GET(self):
+        response = self.client.get(reverse('add_post'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'add_post.html')
+
+    def test_view_add_post_POST(self):
+            with open('media_dir\posts_imgs\david_brown_post_1.jpg', 'rb') as img:
+                response = self.client.post(reverse('add_post'), {
+                    'post_img': img,
+                    'description': 'post test description'
+                })
+
+            post = self.test_user_1.posts.first()    
+            self.test_user_1_profile.refresh_from_db()
+            
+            self.assertEqual(response.status_code, 302)
+            self.assertEqual(post.description, 'post test description')
+            self.assertEqual(self.test_user_1_profile.posts_amount, self.test_user_1.posts.count())
+
+    
+
+    
