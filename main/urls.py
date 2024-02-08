a123aa8f0397258710b87f118ba06f84b42a9e40
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-02-08 15:53:50 +0100

    New include for user app and commenting out urls from main_old app

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("instagram/", include("src.main_old.urls")),
+    # path("instagram/", include("src.main_old.urls")),
+    path("", include("src.user.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
