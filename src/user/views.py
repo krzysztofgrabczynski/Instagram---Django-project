acb1dae4ca4dc613441058e954448f9aa960e347
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-02-08 22:04:46 +0100

    Creat CustomLoginView and home view (for development purposes)

--- src/user/views.py
+++ src/user/views.py
@@ -1,5 +1,7 @@
 from django.views import generic
 from django.contrib.auth.models import User
+from django.contrib.auth import views as auth_views
+from django.http import HttpResponse
 
 from src.user.forms import UserRegistrationForm
 
@@ -9,3 +11,11 @@ class RegisterUserFormView(generic.CreateView):
     form_class = UserRegistrationForm
     template_name = "registration/sign_up.html"
     success_url = ...  # home
+
+
+class CustomLoginView(auth_views.LoginView):
+    redirect_authenticated_user = True
+
+
+def home(request):
+    return HttpResponse("home")
