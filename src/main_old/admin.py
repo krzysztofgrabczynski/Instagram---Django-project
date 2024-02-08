5a8762c556bf0388f7fe1ca44f3099491d1a949d
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-02-08 15:55:55 +0100

    Comment out all files for main_old app

--- src/main_old/admin.py
+++ src/main_old/admin.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
-from .models import UserProfile, Post, Comment, Follow, Like
+# from django.contrib import admin
+# from .models import UserProfile, Post, Comment, Follow, Like
 
 
-admin.site.register(UserProfile)
-admin.site.register(Post)
-admin.site.register(Follow)
-admin.site.register(Comment)
-admin.site.register(Like)
+# admin.site.register(UserProfile)
+# admin.site.register(Post)
+# admin.site.register(Follow)
+# admin.site.register(Comment)
+# admin.site.register(Like)
--- src/main_old/models.py
+++ src/main_old/models.py
@@ -1,84 +1,84 @@
-from django.db import models
-from django.contrib.auth import get_user_model
+# from django.db import models
+# from django.contrib.auth import get_user_model
 
 
-User = get_user_model()
+# User = get_user_model()
 
 
-class UserProfile(models.Model):
-    GENDER = [
-        (0, "male"),
-        (1, "female"),
-    ]
+# class UserProfile(models.Model):
+#     GENDER = [
+#         (0, "male"),
+#         (1, "female"),
+#     ]
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    gender = models.PositiveSmallIntegerField(choices=GENDER, default=0)
-    description = models.TextField(default="", blank=True)
-    profile_img = models.ImageField(
-        default="profile_imgs/default_male.jpg",
-        upload_to="profile_imgs",
-        blank=True,
-        null=True,
-    )
-    posts_amount = models.IntegerField(default=0)
-    followers_amount = models.IntegerField(default=0)
-    following_amount = models.IntegerField(default=0)
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     gender = models.PositiveSmallIntegerField(choices=GENDER, default=0)
+#     description = models.TextField(default="", blank=True)
+#     profile_img = models.ImageField(
+#         default="profile_imgs/default_male.jpg",
+#         upload_to="profile_imgs",
+#         blank=True,
+#         null=True,
+#     )
+#     posts_amount = models.IntegerField(default=0)
+#     followers_amount = models.IntegerField(default=0)
+#     following_amount = models.IntegerField(default=0)
 
-    def __str__(self) -> str:
-        return f"Profile(id:{self.id}): {self.user.username}(id:{self.user.id})"
+#     def __str__(self) -> str:
+#         return f"Profile(id:{self.id}): {self.user.username}(id:{self.user.id})"
 
-    def __repr__(self) -> str:
-        return self.__str__()
-
-
-class Follow(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower")
-    user_followed = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="followed", default=None
-    )
-    followd_user_id = models.PositiveIntegerField(blank=False, default=None)
-
-    def __str__(self) -> str:
-        return f"Follow(id:{self.id}): {self.user.username}(id:{self.user.id}) -----> {self.user_followed.username}(id:{self.user_followed.id})"
-
-    def __repr__(self) -> str:
-        return self.__str__()
+#     def __repr__(self) -> str:
+#         return self.__str__()
 
 
-class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
-    post_img = models.ImageField(upload_to="posts_imgs", blank=False)
-    description = models.TextField(default="")
-    date = models.DateTimeField(blank=False, auto_now_add=True)
-    likes = models.IntegerField(default=0)
+# class Follow(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower")
+#     user_followed = models.ForeignKey(
+#         User, on_delete=models.CASCADE, related_name="followed", default=None
+#     )
+#     followd_user_id = models.PositiveIntegerField(blank=False, default=None)
 
-    def __str__(self) -> str:
-        return f"Post of the: {self.user.username}(id:{self.id})"
+#     def __str__(self) -> str:
+#         return f"Follow(id:{self.id}): {self.user.username}(id:{self.user.id}) -----> {self.user_followed.username}(id:{self.user_followed.id})"
 
-    def __repr__(self) -> str:
-        return self.__str__()
-
-
-class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
-    text = models.TextField(default="")
-    date = models.DateTimeField(blank=False, auto_now=True)
-    likes = models.IntegerField(default=0)
-
-    def __str__(self) -> str:
-        return f"Comment(id:{self.id}) of the: {self.user.username}(id:{self.user.id}) under post(id:{self.post.id}) of the: {self.post.user.username}(id:{self.post.user.id})"
-
-    def __repr__(self) -> str:
-        return self.__str__()
+#     def __repr__(self) -> str:
+#         return self.__str__()
 
 
-class Like(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, blank=True)
+# class Post(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+#     post_img = models.ImageField(upload_to="posts_imgs", blank=False)
+#     description = models.TextField(default="")
+#     date = models.DateTimeField(blank=False, auto_now_add=True)
+#     likes = models.IntegerField(default=0)
 
-    def __str__(self) -> str:
-        return f"Like(id:{self.id}) of the: {self.user.username}(id:{self.user.id}) under post(id:{self.post.id}) of the: {self.post.user.username}"
+#     def __str__(self) -> str:
+#         return f"Post of the: {self.user.username}(id:{self.id})"
 
-    def __repr__(self) -> str:
-        return self.__str__()
+#     def __repr__(self) -> str:
+#         return self.__str__()
+
+
+# class Comment(models.Model):
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+#     text = models.TextField(default="")
+#     date = models.DateTimeField(blank=False, auto_now=True)
+#     likes = models.IntegerField(default=0)
+
+#     def __str__(self) -> str:
+#         return f"Comment(id:{self.id}) of the: {self.user.username}(id:{self.user.id}) under post(id:{self.post.id}) of the: {self.post.user.username}(id:{self.post.user.id})"
+
+#     def __repr__(self) -> str:
+#         return self.__str__()
+
+
+# class Like(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False)
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, blank=True)
+
+#     def __str__(self) -> str:
+#         return f"Like(id:{self.id}) of the: {self.user.username}(id:{self.user.id}) under post(id:{self.post.id}) of the: {self.post.user.username}"
+
+#     def __repr__(self) -> str:
+#         return self.__str__()
