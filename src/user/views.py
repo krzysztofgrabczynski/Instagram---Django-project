f9101a4dd8279696eff905e383e4970fdcb0d5c4
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-02-08 15:51:58 +0100

    Create RegisterUserFormView

--- src/user/views.py
+++ src/user/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.models import User
 
-# Create your views here.
+from src.user.forms import UserRegistrationForm
+
+
+class RegisterUserFormView(generic.CreateView):
+    model = User
+    form_class = UserRegistrationForm
+    template_name = "registration/sign_up.html"
+    success_url = ...  # home
