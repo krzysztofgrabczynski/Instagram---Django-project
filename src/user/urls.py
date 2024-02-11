75e6e35569146c202f64ac594701baffea7a3ef0
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-02-11 23:19:46 +0100

    New url for edit profile

--- src/user/urls.py
+++ src/user/urls.py
@@ -6,18 +6,18 @@ from src.user import views as user_views
 
 urlpatterns = [
     path("sign_up/", user_views.RegisterUserFormView.as_view(), name="sign_up"),
-    path("login/", user_views.CustomLoginView.as_view(), name="login"),
+    path("login/", user_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("home/", user_views.home, name="home"),
-    # path(
-    #     "edit_password/",
-    #     auth_views.PasswordChangeView.as_view(
-    #         template_name="settings/edit_password.html"
-    #     ),
-    #     name="edit_password",
-    # ),
-    # path("", home, name="password_change_done"),
-    # path("edit_account/<int:id>", edit_account, name="edit_account"),
-    # path("edit_profile/<int:id>", edit_profile, name="edit_profile"),
+    path(
+        "edit_password/",
+        user_views.PasswordChangeView.as_view(),
+        name="edit_password",
+    ),
+    path(
+        "edit_profile/<int:pk>/",
+        user_views.EditUserProfileView.as_view(),
+        name="edit_profile",
+    ),
     # path("profile/<int:id>", user_profile, name="user_profile"),
 ]
