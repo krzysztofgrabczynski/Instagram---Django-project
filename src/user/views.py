243ac120c5f84b019d4f9827ac195c0050c02b50
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-02-11 23:19:38 +0100

    New view for edit profile - EditUserProfileView

--- src/user/views.py
+++ src/user/views.py
@@ -1,20 +1,34 @@
+from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.models import User
 from django.contrib.auth import views as auth_views
 from django.http import HttpResponse
 
-from src.user.forms import UserRegistrationForm
+from src.user.forms import UserRegistrationForm, EditUserProfileForm
+from src.user.models import UserProfileModel
 
 
 class RegisterUserFormView(generic.CreateView):
     model = User
     form_class = UserRegistrationForm
     template_name = "registration/sign_up.html"
-    success_url = ...  # home
+    success_url = reverse_lazy("home")
 
 
-class CustomLoginView(auth_views.LoginView):
+class LoginView(auth_views.LoginView):
     redirect_authenticated_user = True
+
+
+class PasswordChangeView(auth_views.PasswordChangeView):
+    template_name = "settings/edit_password.html"
+    success_url = reverse_lazy("home")
+
+
+class EditUserProfileView(generic.edit.UpdateView):
+    model = User
+    form_class = EditUserProfileForm
+    template_name = "settings/edit_profile.html"
+    success_url = reverse_lazy("home")
 
 
 def home(request):
