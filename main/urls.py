bc555bbee00723a7a6656020e9b89b68968330f8
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-02-13 18:42:34 +0100

    Include post.urls in main.urls

--- main/urls.py
+++ main/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     # path("instagram/", include("src.main_old.urls")),
     path("", include("src.user.urls")),
+    path("", include("src.post.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
